1e2f01b74972c4d07cf48990f3df3bdbcfa36b08
Christian Boulanger <c.boulanger@qxtransformer.org>
2011-11-11 23:34:36 +0100

    made attachment example functional

--- examples/find-rename-attachments.js
+++ examples/find-rename-attachments.js
@@ -10,6 +10,8 @@
 
 var bookends    = require('../bookends'),
     file        = require('file'),
+    fs          = require('fs'),
+    prompt      = require('prompt'),
     client      = new bookends.client("localhost",2001),
     
     // configure to your needs:
@@ -18,13 +20,15 @@ var bookends    = require('../bookends'),
     ;
     
 client.enableDebug(false);
+prompt.message = prompt.delimiter = "";
+prompt.start();
 
 var allfiles = [];
 file.walkSync(path, function(path,dirs,files){
     console.log( "Scanning " + path );
     files.forEach(function(file){
         if( file.substr(0,1) == "." ) return;
-        allfiles.push( file );
+        allfiles.push( path + "/" + file );
     });
 });
 
@@ -43,26 +47,64 @@ var counter=0;
 
 function handleFile( file, callback )
 {
-    console.log( " - " + file);
+    console.log( "* " + file);
     var f   = file.split(/\./),
-        key = f[0],
+        key = f[0].substr(f[0].lastIndexOf("/")+1),
         ext = f[1];
     client.query(db,"user1 REGEX '" + key + "'" ,function(err,arr){
-        if( err ) {
-            callback( new Error("ERROR during READ: "+err) ) ;
-        } 
-        else if ( ! arr.length ) {
-            console.log( "  Key '" + key + "' not found." );
-            callback();
+        if( err ) return callback( new Error("ERROR during query: "+err) );
+        if( ! arr.length ) {
+            //console.log( "  Key '" + key + "' not found. Trying author/year/title combination..." );
+            var matches = key.match(/^([^0-9]+?)\-?([0-9]+)\-?(.*?)$/),
+                author  = matches[1].split(/\+/)[0],
+                year    = matches[2],
+                title   = matches[3],
+                query   = "(authors='" + author + "' or editors='" + author +
+                          "') and thedate=" + year + 
+                          ( title ? " and title='" + title + "'" : "");
+            //console.log(query);
+            client.query(db,query,function(err,arr){
+                if( err ) return callback( new Error("ERROR during query: "+err) );
+                if( ! arr.length )
+                {
+                    console.log("  Nothing found.");
+                    return callback();
+                }
+                handleRecord( file, key, ext, arr[0], callback);
+            });
         }
-        else handleRecord( key, ext, arr[0], callback);
+        else handleRecord( file, key, ext, arr[0], callback);
     });
 }
 
-function handleRecord( key, ext, data, callback )
+function handleRecord( file, key, ext, data, callback )
 {
-    console.log( [key,ext,data.user1,data.attachments]);
-    callback();
+    var label   = ( data.authors || data.editors) + " (" + data.thedate + "), " + data.title,
+        oldname = key + "." + ext,
+        newname = data.user1 + "." + ext;
+        
+    console.log( "  Found: "+ label.substr(0,60) );
+    if ( data.user1 !== key )
+    {
+        console.log("  Key '" +data.user1 + "' doesn't match filename!");
+    }
+    
+    // already attached
+    console.log("==>"+data.attachments+":"+oldname);
+    if( typeof data.attachments == "string" && data.attachments.indexOf( oldname ) > -1 )
+    {
+        console.log("  File is already attached to record. ");
+        callback();
+    }
+    else
+    {
+        if ( newname !== oldname ) console.log("  Rename '"+oldname+"' to '" + newname  + "' and attach?");
+        else console.log("  Attach file?");
+        prompt.get([{ name: "answer", message: " [Y]es (default) or [N]o?"}], function (err, result) {
+            //if ( newname !== oldname ) fs.renameSync(path1, path2)
+          callback();
+        });
+    }
 }
 
 function done()
